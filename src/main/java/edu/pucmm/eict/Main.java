6a1a56c556dfe06d0bd45fb58bd4708567c6ce94
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-05-31 18:20:36 -0400

    Agregando metodo para validar la URL ingresada

--- src/main/java/edu/pucmm/eict/Main.java
+++ src/main/java/edu/pucmm/eict/Main.java
@@ -1,9 +1,41 @@
 package edu.pucmm.eict;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         IO.println("Hola");
+
+        Scanner scanner = new Scanner(System.in);
+        String url;
+
+        do{
+            System.out.println("Ingrese una URL: ");
+            url = scanner.nextLine().trim();
+
+        }while(!esURLvalida(url));
+
+
     }
+
+    public static boolean esURLvalida(String url) {
+
+        try {
+            URI uri = new URI(url);
+
+            if (uri.getScheme() != null && uri.getHost() != null) {
+                return true;
+            }
+
+        } catch (URISyntaxException e) {
+            System.out.println("La URL es inválida" + e.getMessage());
+            return false;
+        }
+        return false;
+    }
+
 }
